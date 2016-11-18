06214e218574b442f05bee8092ef51f3af45be17
Alif <habibie.alief@gmail.com>
2016-11-19 00:48:52 +0700

    First

--- app/src/main/java/com/alif/perumahanpekalongan/InfoActivity.java
+++ app/src/main/java/com/alif/perumahanpekalongan/InfoActivity.java
@@ -1,31 +1,42 @@
 package com.alif.perumahanpekalongan;
 
+import android.content.Context;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.alif.perumahanpekalongan.adapter.InfoFragmentPagerAdapter;
+import com.alif.perumahanpekalongan.crud.config.Config;
+import com.alif.perumahanpekalongan.crud.config.Http;
 
 /**
  * Created by Alif on 15/11/2016.
  */
 
 public class InfoActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
 
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
-        viewPager.setAdapter(new InfoFragmentPagerAdapter(getSupportFragmentManager(), InfoActivity.this));
+        InfoFragmentPagerAdapter pagerAdapter = new InfoFragmentPagerAdapter(getSupportFragmentManager(), InfoActivity.this);
+        viewPager.setAdapter(pagerAdapter);
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
--- app/src/main/java/com/alif/perumahanpekalongan/adapter/InfoFragmentPagerAdapter.java
+++ app/src/main/java/com/alif/perumahanpekalongan/adapter/InfoFragmentPagerAdapter.java
@@ -10,8 +10,10 @@ import com.alif.perumahanpekalongan.SelatanFragment;
 import com.alif.perumahanpekalongan.TimurFragment;
 import com.alif.perumahanpekalongan.UtaraFragment;
 
+import org.w3c.dom.Text;
+
 /**
- * Created by Alif on 15/11/2016.
+ * Created by Alif Habibillah.
  */
 
 public class InfoFragmentPagerAdapter extends FragmentPagerAdapter {
